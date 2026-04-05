d6217ff4733164e2b08ce6d12be38777c5bfd616
scarpeline <260343101+scarpeline@users.noreply.github.com>
2026-04-05 13:40:22 -0300

    Adição de barra de progresso de scroll e ajuste de espaçamento

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,16 +5,30 @@ import { TournamentsSection } from "@/components/home/TournamentsSection";
 import { RankingsPreview } from "@/components/home/RankingsPreview";
 import { FeaturesSection } from "@/components/home/FeaturesSection";
 import { CTASection } from "@/components/home/CTASection";
+import { motion, useScroll, useSpring } from "framer-motion";
 
 const Index = () => {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
+
   return (
     <Layout>
+      <motion.div
+        className="fixed top-0 left-0 right-0 h-1 bg-primary z-[60] origin-left"
+        style={{ scaleX }}
+      />
       <HeroSection />
-      <GamesSection />
-      <TournamentsSection />
-      <RankingsPreview />
-      <FeaturesSection />
-      <CTASection />
+      <div className="space-y-10 md:space-y-20">
+        <GamesSection />
+        <TournamentsSection />
+        <RankingsPreview />
+        <FeaturesSection />
+        <CTASection />
+      </div>
     </Layout>
   );
 };
